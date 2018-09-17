2da3f129e0643f908269248e9abf19410b786b36
Jose Junior Almeida <josea@cruzeirodosul.edu.br>
2018-09-17 12:53:02 -0300

    Listando categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Observable } from 'rxjs';
+import { CategoriaService } from '../../services/domain/categoria.service';
 
 @IonicPage()
 @Component({
@@ -8,11 +10,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public categoriaService : CategoriaService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
+    this.categoriaService.findAll().subscribe(
+    response => {
+      console.log(response);
+    },
+    error => {
+      console.log(error)
+    });
   }
 
 }
