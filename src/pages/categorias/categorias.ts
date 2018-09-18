3ec4178b6c2455bb471af6173a47e0df77b98743
Jose Junior Almeida <josea@cruzeirodosul.edu.br>
2018-09-18 16:34:54 -0300

    Mostrando dados e imagem do cliente para página de profile

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Observable } from 'rxjs';
 import { CategoriaService } from '../../services/domain/categoria.service';
 import { CategoriaDTO } from '../../models/categoria.dto';
 import { API_CONFIG } from '../../config/api.config';
