1223d1d05090a7b32c5d4b3b0d5042f777319693
Jose Junior Almeida <josea@cruzeirodosul.edu.br>
2018-09-25 15:40:48 -0300

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -20,6 +20,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
@@ -54,7 +55,7 @@ export class OrderConfirmationPage {
     this.pedidoSercice.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codpedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if(error.status == 403){
@@ -65,6 +66,15 @@ export class OrderConfirmationPage {
 
   back(){
     this.navCtrl.setRoot('CartPage');
+  }
+
+  private extractId(location: string): string{
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
+  }
+
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
