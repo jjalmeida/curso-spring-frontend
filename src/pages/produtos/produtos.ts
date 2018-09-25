e710a4a8834f5df3ef4a10646c5cdf1cd272ab38
Jose Junior Almeida <josea@cruzeirodosul.edu.br>
2018-09-25 15:56:09 -0300

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -13,50 +13,61 @@ export class ProdutosPage {
 
   items: ProdutoDTO[];
 
-  constructor(public navCtrl: NavController, 
-              public navParams: NavParams, 
-              public produtoService: ProdutoService,
-              public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
-    let categoria_id = this.navParams.get("categoria_id");
-
-    let loader = this.presentLoading();
-
-    this.produtoService.findByCategoria(categoria_id)
-    .subscribe(response => {
-      this.items = response['content'];
-      loader.dismiss();
-      this.getImageUrls();
-    },
-    error => {
-      loader.dismiss();
-    });
+    this.loadData();
   }
 
-  getImageUrls(){
-    for(var i=0; i<this.items.length; i++){
+  getImageUrls() {
+    for (var i = 0; i < this.items.length; i++) {
       let item = this.items[i];
 
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
           item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`
         },
-        error => {});
+          error => { });
     }
   }
 
-  showDetail(produto_id){
-    this.navCtrl.push("ProdutoDetailPage",{produto_id : produto_id});
+  showDetail(produto_id) {
+    this.navCtrl.push("ProdutoDetailPage", { produto_id: produto_id });
   }
 
-  presentLoading(){
+  presentLoading() {
     let loader = this.loadingCtrl.create({
       content: "Aguarde..."
     });
 
     loader.present();
     return loader;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
+  }
+
+  loadData() {
+    let categoria_id = this.navParams.get("categoria_id");
+
+    let loader = this.presentLoading();
+
+    this.produtoService.findByCategoria(categoria_id)
+      .subscribe(response => {
+        this.items = response['content'];
+        loader.dismiss();
+        this.getImageUrls();
+      },
+        error => {
+          loader.dismiss();
+        });
   }
 }
