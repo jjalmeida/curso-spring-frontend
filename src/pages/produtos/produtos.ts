8006eb1df764f2f17fa6e39799b51fc16b79ee88
Jose Junior Almeida <josea@cruzeirodosul.edu.br>
2018-09-25 15:49:33 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.services';
 import { API_CONFIG } from '../../config/api.config';
@@ -15,17 +15,24 @@ export class ProdutosPage {
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams, 
-              public produtoService: ProdutoService) {
+              public produtoService: ProdutoService,
+              public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get("categoria_id");
+
+    let loader = this.presentLoading();
+
     this.produtoService.findByCategoria(categoria_id)
     .subscribe(response => {
       this.items = response['content'];
+      loader.dismiss();
       this.getImageUrls();
     },
-    error => {});
+    error => {
+      loader.dismiss();
+    });
   }
 
   getImageUrls(){
@@ -42,5 +49,14 @@ export class ProdutosPage {
 
   showDetail(produto_id){
     this.navCtrl.push("ProdutoDetailPage",{produto_id : produto_id});
+  }
+
+  presentLoading(){
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."
+    });
+
+    loader.present();
+    return loader;
   }
 }
